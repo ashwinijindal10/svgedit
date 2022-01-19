d724a3e9664e63b5dbdef34a64a62c2a566e5f5f
Ashwini <ashwinijindal10@gmail.com>
2022-01-19 15:22:31 +0530

    angle updates

--- src/editor/extensions/ext-helloworld/ext-helloworld.js
+++ src/editor/extensions/ext-helloworld/ext-helloworld.js
@@ -23,41 +23,114 @@ export default {
     const { $id, $click } = svgCanvas;
     let currentCanvasLayer = svgCanvas.getCurrentDrawing().getCurrentLayer();
     const fontSize = 12;
-    const [markerid, arcPrefix, textPrefix, groupPrefix] = [
+    const [
+      textFilterId,
+      markerid,
+      arcPrefix,
+      textPrefix,
+      textPrefixRef,
+      groupPrefix
+    ] = [
+      "textfilterAngleBetween",
       "angleBtwnArrowMarker",
       "arcdegree_",
       "angle_txt_",
+      "angle_txt_ref_",
       "angleGroup_"
     ];
     let startPoints = null;
     let endPoints = null;
     let currentId = null;
 
-    /********************************************/
-    function arc_links(x1, y1, x2, y2, k) {
-      const cx = (x1 + x2) / 2;
-      const cy = (y1 + y2) / 2;
-      const dx = (x2 - x1) / 2;
-      const dy = (y2 - y1) / 2;
-      const dd = Math.sqrt(dx * dx + dy * dy);
-      let ex = cx + (dd ? dy / dd : 0) * k; //* (i - (n - 1) / 2);
-      let ey = cy - (dd ? dx / dd : 0) * k; //* (i - (n - 1) / 2);
-      const controlPoint = calculateControlPoint();
-
-      if (controlPoint) {
-        //https://codepen.io/branneman/pen/BfxjD?editors=1010
-        [ex, ey] = [controlPoint.x, controlPoint.y];
-      }
-
-      return `M${x1} ${y1} Q${ex} ${ey} ${x2} ${y2}`;
-    }
+    /******************** common ************************/
 
     function round(num, decimalPlaces) {
       num = Math.round(num + "e" + decimalPlaces);
       return Number(num + "e" + -decimalPlaces);
     }
 
-    ///////////////////////////////////////////////////////////////////
+    ////////////////////////////// calculation //////////////////////////////////////////////////
+
+    function calculateAngle(
+      { x: Ax1, y: Ay1 },
+      { x: Ax2, y: Ay2 },
+      { x: Bx1, y: By1 },
+      { x: Bx2, y: By2 }
+    ) {
+      const dAx = Ax2 - Ax1;
+      const dAy = Ay2 - Ay1;
+      const dBx = Bx2 - Bx1;
+      const dBy = By2 - By1;
+      let angle = Math.atan2(dAx * dBy - dAy * dBx, dAx * dBx + dAy * dBy);
+      angle = angle * (180 / Math.PI);
+      if (angle < 0) {
+        angle = 180 + angle;
+      }
+
+      return round(angle, 2) ?? 0;
+    }
+
+    function calculateControlPoint() {
+      if (startPoints && endPoints.p3) {
+        const m1 =
+          (startPoints.p1.x - startPoints.p2.x) /
+          (startPoints.p2.y - startPoints.p1.y);
+        let m2;
+
+        if (endPoints.id && startPoints.id !== endPoints.id) {
+          m2 =
+            (endPoints.p1.x - endPoints.p2.x) /
+            (endPoints.p2.y - endPoints.p1.y);
+        } else {
+          m2 = -1 / m1;
+        }
+
+        const c1 = m1 * startPoints.p3.x - startPoints.p3.y;
+        const c2 = m2 * endPoints.p3.x - endPoints.p3.y;
+        const x = (c1 - c2) / (m1 - m2);
+        const y = m1 * x - c1;
+
+        return { x: Math.abs(x) ?? 0, y: Math.abs(y) ?? 0 };
+      }
+
+      return null;
+    }
+
+    //////////////////////////////// drawing obj///////////////////////////////////
+    function createTextFilter() {
+      let filter = $id(textFilterId);
+      if (!filter) {
+        filter = svgCanvas.createSVGElement({
+          element: "filter",
+          attr: {
+            id: textFilterId,
+            x: 0,
+            y: 0,
+            width: 1,
+            height: 1
+          }
+        });
+        const feFlood = svgCanvas.createSVGElement({
+          element: "feFlood",
+          attr: {
+            "flood-color": "#cccc"
+          }
+        });
+
+        const feComposite = svgCanvas.createSVGElement({
+          element: "feComposite",
+          attr: {
+            in: "SourceGraphic",
+            operator: "xor"
+          }
+        });
+        filter.append(feFlood);
+        filter.append(feComposite);
+      }
+      svgCanvas.findDefs().append(filter);
+      return filter;
+    }
+
     function createMarker() {
       let marker = $id(markerid);
       if (!marker) {
@@ -86,171 +159,142 @@ export default {
       }
       return marker;
     }
+
+    function createAngleArc(id, x1, y1, x2, y2, cx, cy) {
+      const path = `M${x1} ${y1} Q${cx ?? 0} ${cy ?? 0} ${x2} ${y2}`;
+      const arcid = arcPrefix + id;
+      let arcObject = $id(arcid);
+      if (!arcObject) {
+        arcObject = svgCanvas.createSVGElement({
+          element: "path",
+          attr: {
+            id: arcid,
+            fill: "none",
+            stroke: "red",
+            "stroke-width": "1",
+            style: "pointer-events:none",
+            "marker-end": `url(#${markerid})`
+          }
+        });
+      }
+
+      arcObject.setAttribute("d", path);
+      return arcObject;
+    }
+
+    function createTextLabel(id, text, x, y) {
+      const txtid = textPrefix + id;
+      let textObject = $id(txtid);
+      if (!textObject) {
+        textObject = svgCanvas.createSVGElement({
+          element: "text",
+          attr: {
+            id: textPrefix + id,
+            fill: "black",
+            stroke: "none",
+            "stroke-width": 0,
+            x,
+            y,
+            "font-size": fontSize,
+            "text-anchor": "start"
+          }
+        });
+      }
+      textObject.innerHTML = text;
+      textObject.setAttribute("x", x);
+      textObject.setAttribute("y", y);
+      return textObject;
+    }
+
+    function createTextRef(id) {
+      const txtrefid = textPrefixRef + id;
+      let textRefObject = $id(txtrefid);
+      if (!textRefObject) {
+        textRefObject = svgCanvas.createSVGElement({
+          element: "use",
+          attr: {
+            id: txtrefid,
+            "xlink:href": `#${textPrefix + id}`,
+            filter: `url(#${textFilterId})`
+          }
+        });
+      }
+      return textRefObject;
+    }
+
+    function createAngleGroup(id) {
+      const grid = groupPrefix + id;
+      let isNew = false;
+      let grObject = $id(grid);
+
+      if (!grObject) {
+        isNew = true;
+        grObject = svgCanvas.createSVGElement({
+          element: "g",
+          attr: {
+            id: grid
+          }
+        });
+      }
+
+      return { grObject, isNew };
+    }
+
     /////////////////////////////////////////////////////////////
 
-    function createDrawElement(id, svgPath, textPosition, text) {
+    function drawAnnotation(id) {
+      createTextFilter();
       createMarker();
-      const g = svgCanvas.createSVGElement({
-        element: "g",
-        attr: {
-          id: groupPrefix + id
-        }
-      });
+      const angleGroupStatus = createAngleGroup(id);
+      const groupObj = angleGroupStatus.grObject;
+      let ctrlPoint = calculateControlPoint();
+      const angleBetween =
+        startPoints.p1 && startPoints.p2 && endPoints.p1 && endPoints.p2
+          ? calculateAngle(
+              startPoints.p1,
+              startPoints.p2,
+              endPoints.p1,
+              endPoints.p2
+            )
+          : 0;
 
-      const pathSVG = svgCanvas.createSVGElement({
-        element: "path",
-        attr: {
-          id: arcPrefix + id,
-          d: svgPath,
-          fill: "none",
-          stroke: "red",
-          "stroke-width": "1",
-          // "stroke-dasharray": "5,5",
-          style: "pointer-events:none",
-          "marker-end": `url(#${markerid})`
-        }
-      });
+      const [center_x, center_y] = [
+        (startPoints.p3.x + endPoints.p3.x) / 2,
+        (startPoints.p3.y + endPoints.p3.y) / 2
+      ];
 
-      const textSvg = svgCanvas.createSVGElement({
-        element: "text",
-        attr: {
-          id: textPrefix + id,
-          fill: "black",
-          stroke: "none",
-          "stroke-width": 0,
-          x: textPosition.x + fontSize * 0.8,
-          y: textPosition.y - 3,
-          "font-size": fontSize,
-          "text-anchor": "start"
-        }
-      });
-      textSvg.innerHTML = text;
-      g.append(textSvg);
-      g.append(pathSVG);
+      const [final_cx, final_cy] = [
+        (center_x + ctrlPoint.x) * 0.5,
+        (center_y + ctrlPoint.y) * 0.5
+      ];
 
-      return g;
+      const angleArcObject = createAngleArc(
+        id,
+        startPoints.p3.x,
+        startPoints.p3.y,
+        endPoints.p3.x,
+        endPoints.p3.y,
+        final_cx,
+        final_cy
+      );
+
+      const textSvg = createTextLabel(
+        id,
+        `${angleBetween}°`,
+        final_cx,
+        final_cy
+      );
+
+      if (angleGroupStatus.isNew) {
+        groupObj.append(createTextRef(id));
+        groupObj.append(textSvg);
+        groupObj.append(angleArcObject);
+        currentCanvasLayer.prepend(groupObj);
+      }
+      return groupObj;
     }
 
     ///////////////////////////////////////////////////////////////////
-    function calculateAngle(
-      { x: Ax1, y: Ay1 },
-      { x: Ax2, y: Ay2 },
-      { x: Bx1, y: By1 },
-      { x: Bx2, y: By2 }
-    ) {
-      const dAx = Ax2 - Ax1;
-      const dAy = Ay2 - Ay1;
-      const dBx = Bx2 - Bx1;
-      const dBy = By2 - By1;
-      let angle = Math.atan2(dAx * dBy - dAy * dBx, dAx * dBx + dAy * dBy);
-      angle = angle * (180 / Math.PI);
-      if (angle < 0) {
-        angle = 180 + angle;
-      }
-
-      return round(angle, 2) ?? 0;
-    }
-
-    function calculateControlPoint() {
-      if (startPoints && endPoints.id) {
-        const m1 =
-          (startPoints.p1.x - startPoints.p2.x) /
-          (startPoints.p2.y - startPoints.p1.y);
-        const m2 =
-          (endPoints.p1.x - endPoints.p2.x) / (endPoints.p2.y - endPoints.p1.y);
-        const c1 = m1 * startPoints.p3.x - startPoints.p3.y;
-        const c2 = m2 * endPoints.p3.x - endPoints.p3.y;
-        const x = (c1 - c2) / (m1 - m2);
-        const y = m1 * x - c1;
-
-        console.log(x, y);
-        const [a1, a2, b1, b2] = [
-          endPoints.p3.x,
-          startPoints.p3.x,
-          endPoints.p3.y,
-          startPoints.p3.y
-        ];
-        const div = a1 * b2 - a2 * b1;
-        const x = (m1 * a1 * b2 - m2 * a2 * b1) / div;
-        const y = (b1 * a2 - m1 * a1 * a2 - a1 * b2 + m2 * a1 * a2) / div;
-        return { x, y };
-      }
-
-      return null;
-    }
-
-    ///////////////////////////////////////////////////////////////////
-
-    function drawArc(id) {
-      if (startPoints && endPoints) {
-        const centerPoint = {
-          x: (startPoints.p3.x + endPoints.p3.x) / 2,
-          y: (startPoints.p3.y + endPoints.p3.y) / 2
-        };
-        const offset = 2;
-        const path = arc_links(
-          startPoints.p3.x + offset,
-          startPoints.p3.y + offset,
-          endPoints.p3.x + offset,
-          endPoints.p3.y + offset,
-          (centerPoint.x + centerPoint.y) / 10
-        );
-
-        const angleBetween = calculateAngle(
-          startPoints.p1,
-          startPoints.p2,
-          endPoints.p1,
-          endPoints.p2
-        );
-
-        const svgGroup = createDrawElement(
-          id,
-          path,
-          centerPoint,
-          `${angleBetween}°`
-        );
-
-        currentCanvasLayer.prepend(svgGroup);
-      }
-    }
-
-    function updateArc() {
-      if (currentId && startPoints && endPoints) {
-        const arcObject = $id(arcPrefix + currentId);
-        const angleTextObject = $id(textPrefix + currentId);
-        let angleBetween = 0;
-        const centerPoint = {
-          x: (startPoints.p3.x + endPoints.p3.x) / 2,
-          y: (startPoints.p3.y + endPoints.p3.y) / 2
-        };
-        const offset = 2;
-        const path = arc_links(
-          startPoints.p3.x + offset,
-          startPoints.p3.y + offset,
-          endPoints.p3.x + offset,
-          endPoints.p3.y + offset,
-          (centerPoint.x + centerPoint.y) / 12
-        );
-
-        if (startPoints.p1 && startPoints.p2 && endPoints.p1 && endPoints.p2) {
-          angleBetween = calculateAngle(
-            startPoints.p1,
-            startPoints.p2,
-            endPoints.p1,
-            endPoints.p2
-          );
-        }
-
-        arcObject.setAttribute("d", path);
-        angleTextObject.innerHTML = `${angleBetween}°`;
-        const textLength = angleTextObject.innerHTML.length;
-        angleTextObject.setAttribute(
-          "x",
-          centerPoint.x + textLength * 2 + fontSize * 0.9
-        );
-        angleTextObject.setAttribute("y", centerPoint.y + 5);
-      }
-    }
 
     return {
       name: svgEditor.i18next.t(`${name}:name`),
@@ -287,7 +331,7 @@ export default {
             startPoints = { id: target.id, p1, p2, p3 };
             endPoints = { ...startPoints };
             currentId = svgCanvas.getNextId();
-            drawArc(currentId);
+            drawAnnotation(currentId);
             return { started: true };
           }
         }
@@ -308,14 +352,14 @@ export default {
             p2 = { x: elem.x2.baseVal.value, y: elem.y2.baseVal.value };
           }
           endPoints = { id, p1, p2, p3 };
-          updateArc();
+          drawAnnotation(currentId);
         }
       },
       mouseUp(opts) {
         if (svgCanvas.getMode() === commandName) {
           if (currentId && !endPoints.id) {
             const groupObjAngleBetween = $id(groupPrefix + currentId);
-            groupObjAngleBetween.remove();
+            if (groupObjAngleBetween) groupObjAngleBetween.remove();
           }
           startPoints = null;
           endPoints = null;
